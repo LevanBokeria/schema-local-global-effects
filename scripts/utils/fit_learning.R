43889118131c4f63756b0df7477798a2461cc404
Levan Bokeria <levan.bokeria@gmail.com>
2025-03-23 12:06:26 +0000

    renamed and documented the fit_learning.R script

---
+++ scripts/utils/fit_learning.R
@@ -0,0 +1,46 @@
+# Fit Learning Model
+# This function fits a learning model to the given data using either a two-parameter
+# or three-parameter exponential decay model. It calculates the predicted values
+# (`y_hat`) and optionally returns either the fitted values or the sum of squared
+# errors (SSE).
+
+# Arguments:
+# - p: A numeric vector of parameters for the model.
+#      For the two-parameter model: p[1] is the scaling factor, p[2] is the decay rate.
+#      For the three-parameter model: p[1] is the asymptote, p[2] is the decay rate, p[3] is the initial value.
+# - t: A numeric vector representing the time points.
+# - y: A numeric vector of observed values.
+# - ret: A character string specifying the return type.
+#        Use 'fit' to return the fitted values (`y_hat`) or any other value to return the sum of squared errors (SSE).
+# - print_output: A logical value indicating whether to print intermediate output.
+# - which_model: A character string specifying the model type.
+#                Use 'two_param' for the two-parameter model or 'three_param' for the three-parameter model.
+
+# Returns:
+# - If ret is 'fit', the function returns a numeric vector of fitted values (`y_hat`).
+# - Otherwise, it returns a numeric value representing the sum of squared errors (SSE).
+
+fit_learning <- function(p, t, y, ret, print_output, which_model) {
+    # Define the function:
+    if (which_model == 'two_param') {
+        y_hat = p[1] * exp(-p[2] * (t - 1))
+    } else if (which_model == 'three_param') {
+        y_hat = p[3] * exp(-p[2] * (t - 1)) + p[1] - p[3]
+    }
+    
+    sse <- sum((y - y_hat)^2)
+
+    if (print_output) {
+        print(paste0('params: ', p))
+        print(paste0('N parameters: ', which_model))
+        print(paste0(y, collapse = ' '))
+        print(paste0(y_hat, collapse = ' '))
+        # print(paste0('sse: ', sse))
+    }
+
+    if (ret == 'fit') {
+        return(y_hat)
+    } else {
+        return(sse)
+    }
+}
